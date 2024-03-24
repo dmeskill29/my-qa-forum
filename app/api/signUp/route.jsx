674a9789bc7bc4b7b7e5a1d4d2fcec5ae729fd57
Dan Meskill <31976624+dmeskill29@users.noreply.github.com>
2024-03-24 19:48:12 -0400

    Refactor user role assignment in signUp route and auth.js

--- app/api/signUp/route.jsx
+++ app/api/signUp/route.jsx
@@ -14,9 +14,28 @@ export async function POST(req) {
     // Starting a transaction if your DB supports it, to ensure both user and account are created
     const result = await db.$transaction(async (prisma) => {
       // First, create the user
-      const user = await prisma.user.create({
-        data: { email, password: hashedPassword, username: nanoid(10)},
-      });
+      if (
+        email === process.env.ADMIN_EMAIL_1 ||
+        email === process.env.ADMIN_EMAIL_2
+      ) {
+        const user = await prisma.user.create({
+          data: {
+            email,
+            password: hashedPassword,
+            username: nanoid(10),
+            roles: ["admin", "user"],
+          },
+        });
+      } else {
+        const user = await prisma.user.create({
+          data: {
+            email,
+            password: hashedPassword,
+            username: nanoid(10),
+            roles: ["user"],
+          },
+        });
+      }
 
       // Then, create the account linked to the user
       const account = await prisma.account.create({
@@ -33,20 +52,26 @@ export async function POST(req) {
     });
 
     // Respond with success message and created entities (consider what you really need to return)
-    return new Response(JSON.stringify({ message: "User and Account Created", result }), {
-      status: 200, // HTTP status code
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    return new Response(
+      JSON.stringify({ message: "User and Account Created", result }),
+      {
+        status: 200, // HTTP status code
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   } catch (error) {
     console.error(error);
     // Respond with error message
-    return new Response(JSON.stringify({ message: "Signup Failed", error: error.message }), {
-      status: 500, // HTTP status code for server error
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    return new Response(
+      JSON.stringify({ message: "Signup Failed", error: error.message }),
+      {
+        status: 500, // HTTP status code for server error
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 }
